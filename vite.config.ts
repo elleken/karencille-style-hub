8ace0399cdb51c6c390e151163f297f2f2707e38
Rasmus Elken <rel@kruso.dk>
2026-03-08 16:59:45 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+   base: '/karencille-style-hub/', // Replace with your repository name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
